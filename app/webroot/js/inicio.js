313ee160e411bdaf01f874ea0c9b9ee7b527ae0e
Ale Prieto <aleprieto@gmail.com>
2012-08-14 19:38:14 -0300

    Alineación de texto.

--- app/webroot/js/inicio.js
+++ app/webroot/js/inicio.js
@@ -3,10 +3,10 @@
  */
 $(document).ready(function() {
 	jwplayer('mediaspace').setup({
-    'flashplayer': 'files/player.swf',
-    'file': 'files/Big_Buck_Bunny_Trailer.webm',
-    'controlbar': 'bottom',
-    'width': '470',
-    'height': '320'
-  });
+		'flashplayer' : 'files/player.swf',
+		'file' : 'files/Big_Buck_Bunny_Trailer.webm',
+		'controlbar' : 'bottom',
+		'width' : '470',
+		'height' : '320'
+	});
 });
